48d97d0fc3ce09627b97d82911fa74c8cfea7767
Gean Ferreira <gean_roberto@hotmail.com>
2023-07-27 17:58:32 -0300

    refactor: Coloca a nova funcionalidade do vue router v4 utilizando slot

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,9 @@
 <template>
-  <RouterView />
+  <router-view v-slot="{ Component, route }">
+    <Transition name="fade" mode="out-in">
+      <section :key="route.path">
+        <component :is="Component"></component>
+      </section>
+    </Transition>
+  </router-view>
 </template>
