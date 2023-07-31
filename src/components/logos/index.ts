fcda9023d031076927d9d3f011944439c737ee18
Gean Ferreira <gean_roberto@hotmail.com>
2023-07-31 18:56:32 -0300

    files: Add imagens da minha marca Evodux

---
+++ src/components/logos/index.ts
@@ -0,0 +1,3 @@
+export { default as HeaderLogoDark } from './HeaderLogoDark.vue'
+export { default as HeaderLogoLight } from './HeaderLogoLight.vue'
+export { default as NameEvodux } from './NameEvodux.vue'
